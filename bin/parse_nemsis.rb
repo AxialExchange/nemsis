b8b09d440b3ac72393d855a3cef7b65392022917
Guangming Cui <gcui@axialexchange.com>
2012-01-18 17:15:33 -0500

    Prototype parse using yaml nemsis spec

--- bin/parse_nemsis.rb
+++ bin/parse_nemsis.rb
@@ -1,92 +1,27 @@
 #!/usr/bin/env ruby
+require 'rubygems'
+require 'ruby-debug'
+require 'yaml'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "nemsis"
 
-require 'rubygems'
-require 'ruby-debug'
-
-$LOAD_PATH.unshift File.expand_path('..', __FILE__)
-require 'mappings'
-
 class Main
-  include Mapping
 
   def self.run
     sample_xml_file = File.expand_path('../../data/sample.xml', __FILE__)
     xml_str = File.read(sample_xml_file)
 
     parser = Nemsis::Parser.new(xml_str)
-    parser.parse
-
-    maps = Mapping.mapping
-    maps.each do |map|
-      name = map[:name]
-      # debugger if name == 'Receiving Application'
-      if name == 'custom' && !map[:nemsis_title_field].nil? && 
-                             !map[:nemsis_value_field].nil?
-
-        xpath = "//#{map[:nemsis_field]}"
-        nodes = parser.find(xpath)
-        nodes.each do |node|
-          field_name  = node.xpath(".//#{map[:nemsis_title_field]}").first.text
-          field_value = node.xpath(".//#{map[:nemsis_value_field]}").first.text
-
-          puts "#{field_name} [#{map[:nemsis_value_field]}]:[#{field_value}]"
-        end
-
-      elsif !map[:nemsis_field].nil? && 
-            map[:nemsis_field].gsub(/\s*/, '').size > 0
-
-        if map[:allow_multiple] == 'Y'
-          nemsis_field = map[:nemsis_field]
-
-          values = []
-          xpath = "//#{nemsis_field}"
-          nodes = parser.find(xpath)
-
-          begin
-            nodes.each do |node|
-              value = node.text rescue nil
-
-              if map[:is_mapped] == 'Y'
-                code_map = Mapping.nemsis_code.merge(map[:map] || {})
-                value = code_map[value]
-              end
-
-              values << value
-              puts "#{name} [#{nemsis_field}]: [#{values.join(' ')}]"
-            end
-          rescue
-          end
-
-        else
-          nemsis_fields = map[:nemsis_field].split(/\s*\+\s*/)
-
-          values = []
-          nemsis_fields.each do |field|
-            xpath = "//#{field}"
-            nodes = parser.find(xpath)
-            begin
-              nodes.each do |node|
-                value = node.text rescue nil
-
-                if map[:is_mapped] == 'Y'
-                  code_map = Mapping.nemsis_code.merge(map[:map] || {})
-                  value = code_map[value]
-                end
-
-                values << value
-              end
-            rescue
-            end
-          end
-
-          puts "#{name} [#{nemsis_fields.join('+')}]: [#{values.join(' ')}]"
-        end
-      else
-        puts "#{name} [default]: [#{map[:default_value]}]"
-      end
-    end
+    puts parser.parse_element('E06_01')
+    puts parser.parse_field('FIRST NAME')
+    puts parser.parse_field('LAST NAME')
+    puts parser.parse_element('E06_02')
+    puts parser.parse_element('E06_16')
+    puts parser.parse_element('E06_11')
+    puts parser.parse_element('E23_09')
+    puts parser.parse_element('E23_11')
+    puts parser.parse_element('E20_02')
 
   end
 
--- lib/nemsis/parser.rb
+++ lib/nemsis/parser.rb
@@ -2,17 +2,58 @@ module Nemsis
   class Parser
     attr_accessor :xml_str, :xml_doc
 
-    def initialize(xml_str)
-      self.xml_str = xml_str
+    class << self
+      attr_accessor :spec
     end
 
-    def parse
+    spec_yaml = File.expand_path('../../../conf/nemsis_spec.yml', __FILE__)
+    @@spec = YAML::load(File.read(spec_yaml))
+
+    def initialize(xml_str)
+      self.xml_str = xml_str
       self.xml_doc = Nokogiri::XML(xml_str)
       xml_doc.remove_namespaces!
     end
 
-    def find(xpath)
-      xml_doc.xpath(xpath)
+    def parse_element(element)
+      element_spec = @@spec[element]
+
+      parse(element_spec)
+    end
+
+    def parse_field(field_name)
+      element_spec = @@spec.values.
+                            select{|v| v['name'] == field_name}.
+                            first
+
+      parse(element_spec)
+    end
+
+    def parse(element_spec)
+      xpath = "//#{element_spec['node']}"
+      nodes = xml_doc.xpath(xpath)
+
+      values = []
+      begin
+        nodes.each do |node|
+          value = node.text
+
+          if element_spec['data_type'] =~ /(text|combo)/i && 
+             value =~ /^\d+$/ && 
+             !element_spec['field_values'].nil?
+
+            mapped_value = element_spec['field_values'][value.to_i]
+            
+            value = mapped_value unless mapped_value.nil?
+          end
+
+          values << value
+        end
+      rescue  error
+        puts "Error: #{error}"
+      end
+
+      values
     end
   end
 end
