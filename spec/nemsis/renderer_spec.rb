56c634826e5b12a92be7813db427a46d5834cc0a
Jon Kern <jonkern@comcast.net>
2012-04-19 15:24:00 -0400

    Moved write_html_file to spec_helper

--- spec/nemsis/renderer_spec.rb
+++ spec/nemsis/renderer_spec.rb
@@ -1,14 +1,5 @@
 require 'spec_helper'
 
-
-def write_html_file(name, type, html)
-  file_name = "#{File.dirname(__FILE__)}/../results/#{name.gsub('.xml','')}-#{Time.now.strftime("%d%b")}_#{type}.html"
-  file = File.open(file_name, 'w+')
-  file.puts html
-  file.close
-  #puts "Open this in your browser to admire your handiwork: file://localhost#{file_name}"
-end
-
 describe Nemsis::Renderer do
   context 'class methods' do
     describe '.initialize' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,14 @@ require 'bundler/setup'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'nemsis'
 
+def write_html_file(name, type, html)
+  file_name = "#{File.dirname(__FILE__)}/results/#{name.gsub('.xml', '')}-#{Time.now.strftime("%d%b")}_#{type}.html"
+  file      = File.open(file_name, 'w+')
+  file.puts html
+  file.close
+  #puts "Open this in your browser to admire your handiwork: file://localhost#{file_name}"
+end
+
+
 RSpec.configure do |config|
 end
